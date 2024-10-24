e057f41db9d3669181a2a9de814da695dc824d82
M-McCorkell156 <148781370+M-McCorkell156@users.noreply.github.com>
2024-10-24 16:09:50 +0100

    nav mesh complete

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public Transform playPos;
     private float speed = 4f;
