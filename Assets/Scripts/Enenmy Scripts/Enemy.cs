dc4e82de049cd38b3bccec1f9495a587c41949bf
M-McCorkell156 <148781370+M-McCorkell156@users.noreply.github.com>
2024-10-28 15:44:01 +0000

    spawning enes complete
    IDamagabke next

--- Assets/Scripts/Enenmy Scripts/Enemy.cs
+++ Assets/Scripts/Enenmy Scripts/Enemy.cs
@@ -3,28 +3,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform playPos;
-    private float speed = 4f;
-    [SerializeField] private Rigidbody rb;
+    public GameObject playerObj;
+    public NavMeshAgent agent;
+    public event EventHandler OnEnemyDeath;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //playPos = GetComponent<>();
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        agent.SetDestination(playerObj.transform.position);
     }
 
-    private void FollowPlayer()
+    private void OnDeath()
     {
-
+        if (OnEnemyDeath != null)
+        {
+            OnEnemyDeath(this, EventArgs.Empty);
+        }
     }
 
     #region Observers 
--- Assets/Scripts/WaveHandler.cs
+++ Assets/Scripts/WaveHandler.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class WaveHandler : MonoBehaviour
 {
+
+    private float waveNo;
+    public float enemyCount;
+
+    public List<Transform> spawnPos;
+    public GameObject enemyPrefab;
+
+    public event EventHandler OnWaveChange;
+    private Enemy Enemy;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        waveNo = 0;
+        WaveChange();
+
+        //Enemy.OnEnemyDeath += Enemy_OnEnemyDeath;
+
+        //Debug.Log($"Start wave No:{waveNo} enemyNo: {enemyCount}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    //Check for remaining enemies
+    public void WaveCheck()
+    {
+        if (enemyCount == 0)
+        {
+            OnWaveChange(this, EventArgs.Empty);
+            WaveChange();
+        }
+
+    }
+
+    //increase wave and calculate no of enemies
+    private void WaveChange()
+    {
+        waveNo++;
+        enemyCount = waveNo * (waveNo + 2) + 2;
+        //Debug.Log($"eneCount{enemyCount}");
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            if (spawnPos != null)
+            {
+                int ranSpawnPos = UnityEngine.Random.Range(1, 4);
+                //Debug.Log($"Random no: {i} spawn: {ranSpawnPos}");
+                Instantiate(enemyPrefab, spawnPos[ranSpawnPos].position, Quaternion.identity);
+            }
+        }
+
+    }
+
+    //Method to call when enemy dies
+    private void Enemy_OnEnemyDeath(object sender, EventArgs e)
+    {
+        Debug.Log("event on enemy death");
+        enemyCount--;
     }
 }
